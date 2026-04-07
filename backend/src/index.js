6c1143746415187b4b0e381cc5c77c85fe9d62c4
Kadir Khan <kk8045433@gmail.com>
2026-04-07 21:33:44 +0530

    some cors issue fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,20 +36,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (Postman, server-to-server)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: true,
     credentials: true,
   })
 );
-
 // ================= ROUTES =================
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
