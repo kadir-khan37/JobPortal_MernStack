80cb4c87dc947c7b5d6ce9ea034cbf8ada35b9ba
Kadir Khan <kk8045433@gmail.com>
2026-04-27 21:40:45 +0530

    fix: add all API endpoints in constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,1 +1,6 @@
-export const User_API_Endpoint = import.meta.env.VITE_API_URL + "/api/v1/user";
+export const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const User_API_Endpoint = `${BASE_URL}/api/v1/user`;
+export const Jobs_API_Endpoint = `${BASE_URL}/api/v1/job`;
+export const Company_API_Endpoint = `${BASE_URL}/api/v1/company`;
+export const Application_API_Endpoint = `${BASE_URL}/api/v1/application`;
