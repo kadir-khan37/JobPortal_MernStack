f037fbf5fb59bb559e0704b56b0b09c9c2b92633
Kadir Khan <kk8045433@gmail.com>
2026-04-29 13:28:30 +0530

    fix: update auth to bearer token

--- backend/src/index.js
+++ backend/src/index.js
@@ -54,5 +54,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port right server  ${PORT}`);
 });
--- backend/src/middlewares/isAuthenticated.js
+++ backend/src/middlewares/isAuthenticated.js
@@ -2,7 +2,12 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token =
+      req.cookies?.token ||
+      req.headers.authorization?.split(" ")[1];
+
+    console.log("COOKIES:", req.cookies);
+    console.log("TOKEN:", token);
 
     if (!token) {
       return res.status(401).json({
@@ -12,13 +17,6 @@ const isAuthenticated = async (req, res, next) => {
     }
 
     const decode = jwt.verify(token, process.env.SECRET_KEY);
-
-    if (!decode) {
-      return res.status(401).json({
-        message: "Invalid token",
-        success: false,
-      });
-    }
 
     req.id = decode.userId;
     next();
