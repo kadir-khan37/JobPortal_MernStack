a983b2f8c197a5342c6adbe61d3226efb1bd98d6
Kadir Khan <kk8045433@gmail.com>
2026-04-29 16:42:44 +0530

    changes

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "@/utils/axios";
+import axios from "../utils/axios";
 import { User_API_Endpoint } from '../utils/constant';
 
 //  Check user session from backend
