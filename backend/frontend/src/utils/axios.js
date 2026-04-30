59c30bc9133d3c44d77122a3fa8d796ce746bba4
Kadir Khan <kk8045433@gmail.com>
2026-04-30 10:05:07 +0530

    changes

--- backend/frontend/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-const formData = new FormData();
-formData.append("name", companyName);
-formData.append("logo", file);
-
-await instance.post("/api/v1/company/register", formData, {
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
-});
--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "../utils/axios";
-import { User_API_Endpoint } from "../utils/constant";
+import axios from "../utils/axios.js";
+import { User_API_Endpoint } from "../utils/constant.js";
 
 //  Check user session from backend
 export const checkAuth = createAsyncThunk(
