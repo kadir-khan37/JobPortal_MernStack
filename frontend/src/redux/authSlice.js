6482ea5b3b40b7e84b94111090fa127c8d00598a
Kadir Khan <kk8045433@gmail.com>
2026-04-30 11:35:01 +0530

    simplify authSlice to fix rollup build error

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,13 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../utils/axios.js";
 
+const API_BASE = "https://jobportal-mernstack-4vux.onrender.com/api/v1/user";
+
+// Check user session from backend
 export const checkAuth = createAsyncThunk(
   "auth/checkAuth",
   async (_, { rejectWithValue }) => {
-    const User_API_Endpoint = "https://jobportal-mernstack-4vux.onrender.com/api/v1/user";
-
     try {
-      const res = await axios.get(`${User_API_Endpoint}/me`, {
+      const res = await axios.get(`${API_BASE}/me`, {
         withCredentials: true,
       });
       return res.data.user;
@@ -23,13 +24,11 @@ const authSlice = createSlice({
     user: null,
     isAuthenticated: false,
   },
-
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true;
     },
-
     logout: (state) => {
       state.user = null;
       state.isAuthenticated = false;
@@ -38,7 +37,6 @@ const authSlice = createSlice({
       state.user = action.payload;
     },
   },
-
   extraReducers: (builder) => {
     builder
       .addCase(checkAuth.fulfilled, (state, action) => {
